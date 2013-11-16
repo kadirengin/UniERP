5f274a62717ac255c38f00d999ebdeb7cce4f51a
Kadir Engin <>
2013-11-16 10:51:48 +0200

    Ogrenci sınıfı oluşturuldu

---
+++ lib/Ogrenci.php
@@ -0,0 +1,14 @@
+<?php
+
+class Ogrenci {
+    
+    public $bolum;
+    public $OgrenciNo;
+    public $sinif;
+    
+    public function notGir($dersKodu,$not){
+        return true;
+        
+    }
+
+    }
